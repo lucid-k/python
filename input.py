4f95deb77cc02e66043345a96d82b35661f1e0ba
lucid-k <maiyokimmant@gmail.com>
2025-06-15 21:34:06 +0300

    display,script and input progs

---
+++ input.py
@@ -0,0 +1,5 @@
+n = input("enter your names")
+a = int(input(f"{n} enter your age"))
+f = float(input("{} enter a random decimal".format(n)))
+bonus = 35000*f
+print("dear: " +n+ "\naged : " +str(a)+ "\nyou win: " +str(bonus)) 
